a0461129058d00ca3e81ce8faafb8167654ef8ba
Yuriy Kurylenko <yuriy.kurylenko@gmail.com>
2017-01-22 12:45:30 +0100

    Password check.

--- app.rb
+++ app.rb
@@ -21,7 +21,11 @@ end
 patch '/users/:id' do
   payload = JSON.parse(request.body.read).symbolize_keys
   @user = User.find(params[:id])
-  @user.update_attributes(payload)
+  @user.update_attribute(:email, payload[:email]) if payload[:email]
+  @user.update_attribute(:fname, payload[:fname]) if payload[:fname]
+  @user.update_attribute(:lname, payload[:lname]) if payload[:lname]
+  @user.update_attribute(:birthday, payload[:birthday]) if payload[:birthday]
+  @user.update_attribute(:password_hash, Digest::MD5.hexdigest(payload[:password])) if payload[:password]
 
   content_type :json
   @user.to_json
@@ -30,5 +34,10 @@ end
 post '/users/check' do
   payload = JSON.parse(request.body.read).symbolize_keys
   @user = User.find_by(email: payload[:email])
-  Digest::MD5.hexdigest(payload[:password])
+
+  if Digest::MD5.hexdigest(payload[:password]) == @user[:password_hash]
+    status 200
+  else
+    status 401
+  end
 end
