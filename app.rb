9accba5083cc7e15a5293685f7ca030ddf4e53fe
Yuriy Kurylenko <yuriy.kurylenko@gmail.com>
2017-01-30 22:40:35 +0100

    Removing extra stuff.

--- app.rb
+++ app.rb
@@ -18,29 +18,17 @@ get '/users' do
   User.all.to_json
 end
 
-patch '/users/:id' do
-  payload = JSON.parse(request.body.read).symbolize_keys
-  @user = User.find(params[:id])
-  @user.update_attribute(:email, payload[:email]) if payload[:email]
-  @user.update_attribute(:role, payload[:role]) if payload[:role]
-  @user.update_attribute(:fname, payload[:fname]) if payload[:fname]
-  @user.update_attribute(:lname, payload[:lname]) if payload[:lname]
-  @user.update_attribute(:birthday, payload[:birthday]) if payload[:birthday]
-  @user.update_attribute(:password_hash, Digest::MD5.hexdigest(payload[:password])) if payload[:password]
-
-  content_type :json
-  @user.to_json
-end
-
 post '/users/login' do
   payload = JSON.parse(request.body.read).symbolize_keys
   @user = User.find_by(email: payload[:email])
 
-  if Digest::MD5.hexdigest(payload[:password]) == @user[:password_hash]
+  if @user.present? && Digest::MD5.hexdigest(payload[:password]) == @user[:password_hash]
     status 200
     content_type :json
     @user.to_json
   else
     status 401
+    content_type :json
+    { success: false }.to_json
   end
 end
